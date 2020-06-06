f08fb88e97fb27ddb9972cac76109f97d4c4e93c
hsnam <robbins.hs@gmail.com>
2020-06-06 19:42:00 +0900

    init

---
+++ src/main/java/com/git/flow/strategy/GitFlowStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.git.flow.strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFlowStrategyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitFlowStrategyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/git/flow/strategy/GitFlowStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.flow.strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFlowStrategyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
